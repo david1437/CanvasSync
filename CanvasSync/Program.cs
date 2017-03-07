c89db17c781720c999444696951748c795111e0f
Griffin Hocker <Griffin Hocker>
2017-03-07 01:18:42 -0500

    The get request code using HttpClient

--- CanvasSync/Program.cs
+++ CanvasSync/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
 
 namespace CanvasSync
 {
@@ -10,6 +13,26 @@ namespace CanvasSync
     {
         static void Main(string[] args)
         {
+            curlRequest("http://httpbin.org/get");
+            Console.ReadLine();
+        }
+
+
+
+        async static void curlRequest(string url)
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                using(HttpResponseMessage response = await client.GetAsync(url))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+
+                        string mycontent = await content.ReadAsStringAsync();
+                        Console.WriteLine(mycontent);
+                    }
+                }
+            }
         }
     }
 }
